cb8cf26e7fad6773916685f0b97b819f5659ab02
Sonny Recio <reciosonny@gmail.com>
2016-10-21 15:17:28 +0800

    initial OOP in JS

---
+++ Object-Oriented/Javascript/inheritance-sample.js
@@ -0,0 +1,29 @@
+/*
+  Note: Inheritance from ECMAScript 6. Not applicable in 5
+*/
+
+"use strict";
+
+class Polygon {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+class Square extends Polygon {
+  constructor(sideLength) {
+    super(sideLength, sideLength);
+  }
+  get area() {
+    return this.height * this.width;
+  }
+  set sideLength(newLength) {
+    this.height = newLength;
+    this.width = newLength;
+  }
+}
+
+var square = new Square(2);
+
+console.log(square);
